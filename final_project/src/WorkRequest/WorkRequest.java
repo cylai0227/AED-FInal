94623dd36888cc3f3eaffa0fbe3535ff6498c617
haohwu <wu.haoh@northeastern.edu>
2023-04-17 14:41:39 -0400

    Update WorkRequest class

---
+++ final_project/src/WorkRequest/WorkRequest.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package WorkRequest;
+
+import Account.Account;
+
+/**
+ *
+ * @author whh
+ */
+public abstract class WorkRequest {
+    static int workId;
+    private String message;
+    private Account sender;
+    private Account receiver;
+    
+    public WorkRequest(){
+        workId++;
+        this.message = message;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public static int getWorkId() {
+        return workId;
+    }
+
+    public static void setWorkId(int workId) {
+        WorkRequest.workId = workId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Account getSender() {
+        return sender;
+    }
+
+    public void setSender(Account sender) {
+        this.sender = sender;
+    }
+
+    public Account getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Account receiver) {
+        this.receiver = receiver;
+    }
+    
+    @Override 
+    public String toString(){
+        return String.valueOf(workId);
+    }
+}
